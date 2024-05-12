e1f9fd39d2461bc8ee02e365952d29edb50208be
aytchhh <huajiehehehe@gmail.com>
2024-05-12 23:10:41 +0100

    add comment deletion feature

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+import { deleteCommentById } from "../../api";
 
-function CommentCard({ comment }) {
+function CommentCard({ comment, user, updateComments }) {
+    const [isDeleting, setIsDeleting] = useState(false)
+    const handleDelete = (()=>{
+        if(window.confirm('Delete this comment?')){
+            setIsDeleting(true)
+            deleteCommentById(comment.comment_id)
+                .then(()=>{
+                    updateComments(comment.comment_id)
+                    setTimeout(() => {
+                        window.alert("Comment deleted!")
+                    }, 400);
+                })
+                .catch((err)=>{
+                    setIsDeleting(false)
+                    window.alert(err.response.data.message)
+                })
+        }
+    })
+
     return (
         <div className="comment-card">
             <p>author: {comment.author}</p>
             {/* to be refactored 👇*/}
             <p>created at: {comment.created_at}</p>
             <p>{comment.body}</p>
+            {
+                (comment.author===user.username) &&
+                <button className="delete-comment-button" onClick={handleDelete} disabled={isDeleting}>&#215;</button>
+            }
         </div>
     )
 }
